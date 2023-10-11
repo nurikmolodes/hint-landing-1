e6ac31448bccd5a678519e8c0bfd4c6c0aea619c
Nurik <102224735+nurikmolodes@users.noreply.github.com>
2023-10-11 16:51:54 +0600

    done

--- src/Pages/genderPage/GenderPage.tsx
+++ src/Pages/genderPage/GenderPage.tsx
@@ -1,38 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import './GenderPage.scss';
-import {Female, Male} from "../../assets/images/iconPack";
+import "./GenderPage.scss";
+import { Female, Male } from "../../assets/images/iconPack";
 import TileItem from "../../components/tileSelector/TileItem";
 import GoBack from "../../components/goBack/GoBack";
-import {useHistory} from "react-router-dom";
-import {DateOfBirth, localStorageItems} from "../../utils/constants";
-import {pushToLocalStorage} from "../../utils/functions";
+import { useHistory } from "react-router-dom";
+import { DateOfBirth, localStorageItems } from "../../utils/constants";
+import { pushToLocalStorage } from "../../utils/functions";
 
 const GenderPage = () => {
   const history = useHistory();
 
-  const [active, setActive] = useState<'male' | 'female' | null>(null);
+  const [active, setActive] = useState<"Male" | "Female" | null>(null);
 
-  const changeGender = (data: 'male' | 'female') => {
+  const changeGender = (data: "Male" | "Female") => {
     setActive(data);
-    pushToLocalStorage({value: data, itemName: localStorageItems.GENDER})
+    pushToLocalStorage({ value: data, itemName: localStorageItems.GENDER });
     history.push(DateOfBirth);
-  }
+  };
   return (
-    <div className={'gender'}>
+    <div className={"gender"}>
       <GoBack />
-      <h1 className={'gender__title'}>What’s your gender?</h1>
-      <div className={'gender__container'}>
+      <h1 className={"gender__title"}>What’s your gender?</h1>
+      <div className={"gender__container"}>
         <TileItem
-          onClick={() => {changeGender("male")}}
-          active={active === 'male'}
-          title={'Male'}
+          onClick={() => {
+            changeGender("Male");
+          }}
+          active={active === "Male"}
+          title={"Male"}
           Icon={() => <Male />}
         />
         <TileItem
-          onClick={() => {changeGender("female")}}
-          active={active === 'female'}
-          title={'Female'}
+          onClick={() => {
+            changeGender("Female");
+          }}
+          active={active === "Female"}
+          title={"Female"}
           Icon={() => <Female />}
         />
       </div>
--- src/services/fetchCompability/index.ts
+++ src/services/fetchCompability/index.ts
@@ -1,27 +1,29 @@
-import {axiosInstance as axios} from "../api";
-import {localStorageItems} from "../../utils/constants";
+import { axiosInstance as axios } from "../api";
+import { localStorageItems } from "../../utils/constants";
 
 export const fetchCompability = async () => {
-  const local = localStorage.getItem('userInfo');
+  const local = localStorage.getItem("userInfo");
   if (local) {
     const storageData = JSON.parse(local);
     const body = {
-      yourGender: storageData[localStorageItems.GENDER],
-      yourInfo: {
-        dateOfBirth: storageData[localStorageItems.DATE_OF_BIRTH],
-        timeOfBirth: storageData[localStorageItems.TIME_OF_BIRTH],
-      },
-      partnerInfo: {
-        dateOfBirth: storageData[localStorageItems.PARTNER_DATE_OF_BIRTH],
-        timeOfBirth: storageData[localStorageItems.PARTNER_TIME_OF_BIRTH],
-      }
+      name: "Unknown",
+      email: storageData[localStorageItems.EMAIL],
+      gender: storageData[localStorageItems.GENDER],
+      dateOfBirth: storageData[localStorageItems.DATE_OF_BIRTH],
+      lat: "-12.000",
+      lon: "22.0",
     };
 
     try {
-      const data = await axios.post('api/astro/compatibility', JSON.stringify(body))
+      const data = await axios.post("api/users/registration", body, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      });
+      window.location.replace("https://astrology-one.vercel.app");
       console.log(data);
     } catch (e) {
       console.error(e);
     }
   }
-}
+};

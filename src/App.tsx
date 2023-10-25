1a5a85918436395524be253f28eca6691d4851fb
Nurik <102224735+nurikmolodes@users.noreply.github.com>
2023-10-25 13:13:04 +0600

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React, {useEffect, useState} from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
 import Header from "./components/Header/Header";
 
-import './App.css';
-import {PayWall} from "./utils/constants";
+import "./App.css";
+import { PayWall } from "./utils/constants";
 
 function App() {
   const [loc, setLoc] = useState(false);
 
   useEffect(() => {
     setLoc(window.location.href.includes(PayWall));
-  }, [window.location.href]);
+  }, []);
   return (
-      <BrowserRouter>
-        {
-          !loc
-            ? <Header />
-            : null
-        }
-        <AppRouter />
-      </BrowserRouter>
+    <BrowserRouter>
+      {!loc ? <Header /> : null}
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
